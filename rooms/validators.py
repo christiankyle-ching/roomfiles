9f3e0e9e7dbf8b4b7ce481360f25165924ecbe85
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2020-07-21 18:10:42 +0800

    Added: Timer before Closing Account

--- rooms/validators.py
+++ rooms/validators.py
@@ -3,10 +3,10 @@ from django.core.exceptions import ValidationError
 import re
 
 def limit_file_size(file):
-    if int(file.size) > MAX_FILE_SIZE:
+    if int(file.size) > settings.MAX_FILE_SIZE:
         raise ValidationError(f'File size exceeds { settings.MAX_FILE_SIZE / (1024**2) }MB limit')
 
 def allowed_file_type(file):
     file_type = re.search('\.([0-9a-z]+)$', file.name, re.I).group(1)
     if file_type.lower() not in settings.ALLOWED_FILE_TYPES:
-        raise ValidationError(f'File type "{file_type}" is not supported')
+        raise ValidationError(f'File type "{file_type.upper()}" is not supported')
